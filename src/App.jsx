7bdac2007b8f343600521e4f41637249ae191f4e
Indrachapa Jayasuriya <155380659+DewminiIndrachapa@users.noreply.github.com>
2025-12-13 23:08:12 +0530

    day12

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import ProductCard from './components/ProductCard.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AdminPage from './pages/AdminPage.jsx'
+import LoginPage from './pages/loginPage.jsx'
+import RegisterPage from './pages/registerPage.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-     <div className='w-[600px] h-[600px] border bg-gray-400'>
-         <div className='w-[500px] h-[500px] border bg-yellow-200 flex  flex-col justify-center items-center'>
-             <div className='w-[100px] h-[100px] border bg-blue-700'></div>
-              <div className='w-[100px] h-[100px] border bg-green-700'></div>
-              <div className='w-[100px] h-[100px] border bg-red-700 fixed left-[550px] top-[550px]'></div>
-              <div className='w-[100px] h-[100px] border absolute left-[300px] bg-purple-700'></div>
-         </div>
-
-
-     </div>
-    </>
+  
+    <BrowserRouter>
+     <div className="w-full h-screen bg-red-500">
+      <Routes path="/">
+        <Route path="/" element ={<h1>Home</h1>} />
+        <Route path="/admin" element ={<AdminPage/>} />
+        <Route path="/login" element ={<LoginPage/>} />
+        <Route path="/register" element ={<RegisterPage/>} />
+   
+      </Routes>
+      </div>
+     </BrowserRouter>
+  
   )
 }
 
